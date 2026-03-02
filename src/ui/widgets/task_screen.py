10b49573e0ef9c6b3015542df4554f457db00669
Philip Kock <philip.kock@stud.tu-darmstadt.de>
2026-03-02 21:41:00 +0100

    implemented time picker for end_time

--- src/ui/widgets/task_screen.py
+++ src/ui/widgets/task_screen.py
@@ -4,9 +4,11 @@ from ui.mq_resources import ListTaskItem
 
 from datetime import datetime, time
 
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 from kivymd.uix.pickers import MDModalDatePicker, MDTimePickerDialVertical
 from kivymd.uix.screen import MDScreen
 
+from kivy.metrics import dp
 from kivy.lang import Builder
 Builder.load_file("ui/widgets/task_screen.kv")
 
@@ -45,11 +47,18 @@ class TaskScreen(MDScreen):
         if self.task.end_time:
             self.ids.end_time_button_text.text =\
                 "End: " + self.task.end_time.strftime(Config.get("time_format"))
+            self.end_time_dialog =\
+                MDTimePickerDialVertical(hour=str(self.task.end_time.hour),
+                                         minute=str(self.task.end_time.minute))
+        else:
+            self.end_time_dialog = MDTimePickerDialVertical()
 
         self.date_dialog.bind(on_ok=self.confirm_date_selection)
         self.date_dialog.bind(on_cancel=self.date_dialog.dismiss)
-        self.start_time_dialog.bind(on_ok=self.confirm_start_time_selection)
+        self.start_time_dialog.bind(on_ok=self.confirm_time_selection)
         self.start_time_dialog.bind(on_cancel=self.start_time_dialog.dismiss)
+        self.end_time_dialog.bind(on_ok=self.confirm_time_selection)
+        self.end_time_dialog.bind(on_cancel=self.start_time_dialog.dismiss)
 
         # setup subtasks
         self.ids.subtask_list.clear_widgets()
@@ -63,12 +72,19 @@ class TaskScreen(MDScreen):
         ]
         self.date_dialog.open()
 
-    def open_start_time_selector(self):
-        self.start_time_dialog.pos = [
-            self.center_x - self.start_time_dialog.width / 2,
-            self.y,
-        ]
-        self.start_time_dialog.open()
+    def open_time_selector(self, idx: int) -> None:
+        if idx == 0:
+            self.start_time_dialog.pos = [
+                self.center_x - self.start_time_dialog.width / 2,
+                self.y,
+            ]
+            self.start_time_dialog.open()
+        elif idx == 1:
+            self.end_time_dialog.pos = [
+                self.center_x - self.end_time_dialog.width / 2,
+                self.y,
+            ]
+            self.end_time_dialog.open()
 
     def confirm_date_selection(self, date_dialog: MDModalDatePicker) -> None:
         if self.task.date is None:
@@ -82,22 +98,37 @@ class TaskScreen(MDScreen):
         self.update_widgets()
         date_dialog.dismiss()
 
-    def confirm_start_time_selection(self, start_time_dialog: MDTimePickerDialVertical) -> None:
-        hour: int = int(start_time_dialog.hour)
-        if start_time_dialog.am_pm == "pm":
+    def confirm_time_selection(self, time_dialog: MDTimePickerDialVertical) -> None:
+        hour: int = int(time_dialog.hour)
+        if time_dialog.am_pm == "pm":
             hour = (hour + 12) % 24 if hour > 0 else 0
-        minute: int = int(start_time_dialog.minute)
+        minute: int = int(time_dialog.minute)
 
-        self.task.start_time = time(hour, minute)
+        if time_dialog == self.start_time_dialog:
+            self.task.start_time = time(hour, minute)
 
-        # if there is no endtime yet, set it to one hour later
-        if self.task.end_time is None:
-            if hour < 23:
-                self.task.end_time = time(hour + 1, minute)
+            # if there is no endtime yet, set it to one hour later
+            if self.task.end_time is None:
+                if hour < 23:
+                    self.task.end_time = time(hour + 1, minute)
+                else:
+                    self.task.end_time = time(23, 59)
+            self.update_widgets()
+            time_dialog.dismiss()
+        else:
+            if self.task.start_time <= time(hour, minute):
+                self.task.end_time = time(hour, minute)
+                self.update_widgets()
+                time_dialog.dismiss()
             else:
-                self.task.end_time = time(23, 59)
-        self.update_widgets()
-        start_time_dialog.dismiss()
+                MDSnackbar(
+                    MDSnackbarText(
+                        text="The task can't end before it began.",
+                    ),
+                    y=dp(24),
+                    pos_hint={"center_x": 0.5},
+                    size_hint_x=0.9,
+                ).open()
 
     def update_description(self):
         self.task.description = self.ids.description_field.text
